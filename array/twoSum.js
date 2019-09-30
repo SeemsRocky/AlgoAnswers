448ad39911ec849cec373e32e1a16719b837e229
SeemsRocky <rliao3613@gmail.com>
2019-09-30 11:53:25 -0400

    added brute and properly fixed optimization

--- array/twoSum.js
+++ array/twoSum.js
@@ -5,8 +5,18 @@
  */
 const twoSum = function(nums, target) {
   const cache = {};
-  for(const i in nums){
-    if(cache[target-nums[i]]) return [cache[target-nums[i]],i];
-    if(cache[nums[i]]===undefined) cache[nums[i]]=i;
+  for(const val of nums){
+    if(cache.hasOwnProperty(target-val)) return [cache[target-val],val];
+    if(!cache.hasOwnProperty(nums[val])) cache[val]=val;
   }
 };
+console.log(twoSum([1,22,-13,4,3,5,9,-5],6))
+
+const bruteTwoSum = function(nums, target){
+  for(let i = 0; i < nums.length; i += 1){
+    for(let j = i; j < nums.length; j += 1){
+      if(nums[j]+nums[i]===target) return [nums[i],nums[j]] // or [i,j] for indices
+    }
+  }
+}
+console.log(bruteTwoSum([22,-13,4,1,3,5,9,-5],6))
