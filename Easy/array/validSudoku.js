e6c7f83357d255aab2efb1bb94f53a00e7f1010b
SeemsRocky <rliao3613@gmail.com>
2020-03-03 16:16:22 -0500

    valid sudoku solution

---
+++ Easy/array/validSudoku.js
@@ -0,0 +1,37 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+
+var isValidSudoku = function(board) {
+  const columns = {}
+  const rows = {}
+  const boxes = {}
+  let isValid = true
+  function checkConstraint(constraint,constraintIndex,currentNum) {
+      if(constraint.hasOwnProperty(constraintIndex)){
+          if(constraint[constraintIndex][currentNum]){
+              console.log(constraint,constraintIndex,currentNum)
+              isValid = false    
+          }
+      } else {
+          constraint[constraintIndex] = new Array(9);
+      }
+      constraint[constraintIndex][currentNum] = currentNum;
+  }
+  for(let row = 0; row < board.length; row +=1 ){
+      for(let col = 0; col < board[row].length; col += 1){
+          const currentNum = Number(board[row][col])
+          if(currentNum){
+              checkConstraint(columns,col,currentNum)
+              checkConstraint(rows,row,currentNum)
+              checkConstraint(boxes,(Math.floor(row/3)*3 + (Math.floor(col/3))),currentNum)
+              if(!isValid){
+                  return false
+              }
+          }
+          
+      }
+  }
+  return true;
+};
